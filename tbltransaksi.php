40a1713147d43295876e77e8e1d2d3100510e939
RIF-creativity <126873296+RIF-creativity@users.noreply.github.com>
2023-11-21 11:59:18 +0700

    two

--- tbltransaksi.php
+++ tbltransaksi.php
@@ -1,11 +1,22 @@
+<?php include 'admin/pages/admin/layout/navbar.php'; ?>
+<?php include 'admin/pages/admin/layout/sidebar.php'; ?>
+<?php 
+session_start()
+if (!isset($_SESSION['login'])) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabel Produk</title>
+    <title>Tabel Transaksi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="admin/css/style.css">
 
     <style>
         .container {
@@ -18,15 +29,15 @@
         }
 
         .table {
-            background-color: #cfe2ff
+            background-color: #cfe2ff;
         }
 
         .table thead tr {
-            border-bottom: 2px solid #c4d7f4
+            border-bottom: 2px solid #c4d7f4;
         }
 
         .table tbody tr {
-            border-bottom: 2px solid #c4d7f4
+            border-bottom: 2px solid #c4d7f4;
         }
 
         #search {
@@ -51,7 +62,7 @@
 <body>
     <div class="container mt-4">
         <div class="row">
-            <h1>Produk <span class="p">jumlah</span></h1>
+            <h1>Transaksi <span class="p">jumlah</span></h1>
         </div>
     </div>
     <div class="container">
@@ -85,9 +96,10 @@
                         <thead>
                             <tr>
                                 <th scope="col"><center>Nomor</center></th>
-                                <th scope="col"><center>Nama</center></th>
-                                <th scope="col"><center>Jumlah</center></th>
-                                <th scope="col"><center>Harga</center></th>
+                                <th scope="col"><center>ID Transaksi</center></th>
+                                <th scope="col"><center>Pembeli</center></th>
+                                <th scope="col"><center>Alamat</center></th>
+                                <th scope="col"><center>Menu</center></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -96,50 +108,86 @@
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=1" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=1" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
                             <tr>
                                 <th scope="row"><center>2</center></th>
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=2" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=2" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
                             <tr>
                                 <th scope="row"><center>3</center></th>
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=2" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=2" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
                             <tr>
                                 <th scope="row"><center>4</center></th>
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=2" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=2" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
                             <tr>
                                 <th scope="row"><center>5</center></th>
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=2" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=2" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
                             <tr>
                                 <th scope="row"><center>6</center></th>
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=2" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=2" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
                             <tr>
                                 <th scope="row"><center>7</center></th>
                                 <td><center></center></td>
                                 <td><center></center></td>
                                 <td><center></center></td>
-                                <td><center></center></td>
+                                <td>
+                                    <center>
+                                        <a href="edit_transaction.php?id=2" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="view_transaction.php?id=2" class="btn btn-info btn-sm"><i class="fas fa-eye"></i> View</a>
+                                    </center>
+                                </td>
                             </tr>
+                            <!-- Add more transaction rows as needed -->
                         </tbody>
                     </table>
                 </div>
